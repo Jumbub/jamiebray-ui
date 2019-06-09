cfcd7d852df22142b1c09313b5203247f91334bc
Jamie Bray <jamie@jamiebray.xyz>
2019-06-09 22:31:49 +0800

    Renamed App component story label to Pages

--- src/components/App/App.story.tsx
+++ src/components/App/App.story.tsx
@@ -5,5 +5,5 @@ import App from './App'
 // Import the global css
 import '../../index.css';
 
-storiesOf('Components', module)
+storiesOf('Pages', module)
   .add('https://jamiebray.me', () => <App />)
