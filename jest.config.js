92bfd66de364ba7625f0830093fbe2424fa4ddb8
Jamie Bray <jamie@jamiebray.xyz>
2019-06-18 22:44:36 +0800

    Added jest file mock for raster images

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,10 @@ module.exports = {
     "^.+\\.svg?$": "jest-svg-transformer",
     "^.+\\.scss?$": "jest-css-modules-transform"
   },
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+     "<rootDir>/__mocks__/fileMock.js",
+  },
   "setupFilesAfterEnv": [
     "<rootDir>/configs/setupTests.js"
   ],
