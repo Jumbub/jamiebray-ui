9cd67ace784ee3bbe26c6ff0f78f68593e808c48
Jamie Bray <jamie@jamiebray.xyz>
2019-06-18 22:45:54 +0800

    Deleted commented code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './App.module.scss'
 import NavigationBar from '../NavigationBar/NavigationBar'
 import ContentCardAbout from '../ContentCardAbout/ContentCardAbout'
-import ContentCardContact from '../ContentCardContact/ContentCardContact'
 import { FULL_NAME } from '../../constants/personal'
 
 const App: React.FC = () => {
@@ -12,12 +11,10 @@ const App: React.FC = () => {
         title={FULL_NAME}
         links={[
           { label: 'About', href: '/about' },
-          // { label: 'Contact', href: '/contact' },
         ]}
       />
       <div className={styles.inner}>
         <ContentCardAbout />
-        {/* <ContentCardContact /> */}
       </div>
     </div>
   )
