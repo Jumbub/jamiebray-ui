3da37c23061db6bf94bf7263b3f7a4e32c243537
Jamie Bray <jamie@jamiebray.xyz>
2019-06-22 00:47:22 +0800

    Added router to app, and rendering contact or about page given url

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,19 +3,28 @@ import styles from './App.module.scss'
 import NavigationBar from '../NavigationBar/NavigationBar'
 import ContentCardAbout from '../ContentCardAbout/ContentCardAbout'
 import { FULL_NAME } from '../../constants/personal'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import ContentCardContact from '../ContentCardContact/ContentCardContact';
 
 const App: React.FC = () => {
   return (
     <div className={styles.outer}>
-      <NavigationBar
-        title={FULL_NAME}
-        links={[
-          { label: 'About', href: '/about' },
-        ]}
-      />
-      <div className={styles.inner}>
-        <ContentCardAbout />
-      </div>
+      <BrowserRouter>
+        <NavigationBar
+          title={FULL_NAME}
+          links={[
+            { label: 'About', href: '/about' },
+            { label: 'Contact', href: '/contact' },
+          ]}
+        />
+        <div className={styles.inner}>
+          <Switch>
+            <Route path="/about" component={ContentCardAbout}/>
+            <Route path="/contact" component={ContentCardContact}/>
+            <Route component={ContentCardAbout} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
