45c888e0757dff258629fabad467691c4c09a409
Jamie Bray <jamie@jamiebray.xyz>
2019-06-22 19:06:05 +0800

    Added google drive iframe'd resume document to resume card

--- src/components/ContentCardResume/ContentCardResume.tsx
+++ src/components/ContentCardResume/ContentCardResume.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from 'react'
 import styles from './ContentCardResume.module.scss'
-import Card from '../Card/Card';
-import Content from '../Content/Content';
-import { FaDownload } from 'react-icons/fa';
-import { RESUME_PDF_DOWNLOAD } from '../../constants/personal';
+import Card from '../Card/Card'
+import Content from '../Content/Content'
+import { FaDownload } from 'react-icons/fa'
+import { RESUME_PDF_DOWNLOAD } from '../../constants/personal'
 
 const ContentCardResume: React.FC = () => {
   return (
     <Card>
       <Content title="Resume">
         <div className={styles.textAndAction}>
-          <p>
-            You can view my resume here, or download it!
-          </p>
+          <p>You can view my resume here, or download it!</p>
           <a href={RESUME_PDF_DOWNLOAD}>
             <Card className={styles.iconCard}>
               <FaDownload className={styles.icon} />
             </Card>
           </a>
         </div>
+        <Card className={styles.documentWrapper}>
+          <iframe
+            src="https://docs.google.com/document/d/e/2PACX-1vSRoB7vomuVYqdlVbF49GwDtgjuCXpytklI8upjOQ5-CTcZfLutDBeUs7ZtnLsM60ySe-nbupFz1UPK/pub?embedded=true"
+            className={styles.document}
+          />
+        </Card>
       </Content>
     </Card>
   )
