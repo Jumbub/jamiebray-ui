cbef3c807ff658d04c11d083cdd5eabedd55a4c6
Jamie Bray <jamie@jamiebray.xyz>
2019-06-22 22:58:33 +0800

    Added storybook knobs to content component

--- src/components/Content/Content.story.tsx
+++ src/components/Content/Content.story.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Content from './Content'
 import MockContent from '../../mocks/MockContent'
+import { text, withKnobs } from '@storybook/addon-knobs';
 
-storiesOf('Components', module).add('Content', () => (
-  <Content title="Title">
+storiesOf('Components', module)
+  .addDecorator(withKnobs)
+  .add('Content', () => (
+  <Content title={text('title', 'Example Title')}>
     <MockContent />
   </Content>
 ))
