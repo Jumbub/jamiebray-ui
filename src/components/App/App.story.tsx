1d71a3807bbe67f9df4bfb4c97bda1952586cd1e
Jamie Bray <jamie@jamiebray.xyz>
2019-06-22 23:47:58 +0800

    Added accessbility decorator to app component

--- src/components/App/App.story.tsx
+++ src/components/App/App.story.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y';
 import App from './App'
 
 storiesOf('Content', module)
+  .addDecorator(withA11y)
   .add('App', () => <App />)
