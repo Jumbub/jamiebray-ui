07e7682948298813f4ab88cf16c26ba6973d9bc7
Jamie Bray <jamie@jamiebray.xyz>
2019-06-23 01:03:36 +0800

    Added dark theme to storybook

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,13 @@
 import { withA11y } from '@storybook/addon-a11y';
-import { configure, addDecorator } from '@storybook/react';
+import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { themes } from '@storybook/theming'
+
+addParameters({
+  options: {
+    theme: themes.dark
+  }
+})
 
 const req = require.context('../src/components', true, /\.story\.tsx$/);
 
