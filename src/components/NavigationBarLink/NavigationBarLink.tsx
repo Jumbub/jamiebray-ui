da39dd9b4b33f8e378f4c4776a43aba8e718db98
Jamie Bray <jamie@jamiebray.xyz>
2019-06-23 23:07:28 +0800

    Added card wrapper style when navigation route matches link href

--- src/components/NavigationBarLink/NavigationBarLink.tsx
+++ src/components/NavigationBarLink/NavigationBarLink.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import styles from './NavigationBarLink.module.scss'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
+import Card from '../Card/Card'
+import useReactRouter from 'use-react-router'
 
 const NavigationBarLink: React.FC<{
   label: string
   href: string
   className: string
   onClick?: () => void
-}> = ({ label, href, className, onClick }) => (
-  <Link
-    onClick={() => (onClick && onClick()) || {}}
-    to={href}
-    key={href}
-    className={className}
-  >
-    {label}
-  </Link>
-)
+}> = ({ label, href, className, onClick }) => {
+  const { location } = useReactRouter();
 
+  const link = (
+    <Link
+      onClick={() => (onClick && onClick()) || {}}
+      to={href}
+      key={href}
+      className={className}
+    >
+      {label}
+    </Link>
+  )
+
+  return location.pathname === href ? <Card>{link}</Card> : link
+}
 export default NavigationBarLink
