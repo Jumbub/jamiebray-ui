8794115d36c7e4ed65c84a22d2c73d7f7d453d28
Jamie Bray <jamie@jamiebray.xyz>
2019-06-23 21:41:51 +0800

    Added a mobile friendly drawer icon button for mobile users

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,21 +2,28 @@ import React from 'react'
 import styles from './NavigationBar.module.scss'
 import Card from '../Card/Card'
 import { Link } from 'react-router-dom'
+import useIsMobile from '../../hooks/useIsMobile'
+import { FaGripLines } from 'react-icons/fa'
 
 const NavigationBar: React.FC<{
   title: string
   links: { label: string; href: string }[]
 }> = ({ title, links }) => {
+  const isMobile = useIsMobile()
   return (
     <Card className={styles.card}>
       <div className={styles.container}>
         <div className={styles.title}>{title}</div>
         <div className={styles.links}>
-          {links.map(({ label, href }) => (
-            <Link to={href} key={href} className={styles.link}>
-              {label}
-            </Link>
-          ))}
+          {isMobile ? (
+            <FaGripLines className={`${styles.link} ${styles.skinnyLink}`}/>
+          ) : (
+            links.map(({ label, href }) => (
+              <Link to={href} key={href} className={styles.link}>
+                {label}
+              </Link>
+            ))
+          )}
         </div>
       </div>
     </Card>
