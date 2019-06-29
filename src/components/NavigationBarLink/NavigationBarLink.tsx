cf4d89341dd9519adf3faa5300dfec71506b808d
Jamie Bray <jamie@jamiebray.xyz>
2019-06-29 22:14:08 +0800

    Disabled pointer events on currently active location link

--- src/components/NavigationBarLink/NavigationBarLink.tsx
+++ src/components/NavigationBarLink/NavigationBarLink.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Card from '../Card/Card'
 import styles from './NavigationBarLink.module.scss'
 import useReactRouter from 'use-react-router'
+import classNames from 'classnames'
 
 const NavigationBarLink: React.FC<{
   label: string
@@ -10,19 +11,22 @@ const NavigationBarLink: React.FC<{
   className?: string
   onClick?: () => void
 }> = ({ label, href, className, onClick }) => {
-  const { location } = useReactRouter();
+  const { location } = useReactRouter()
+  const activeLocation = location.pathname === href
 
   const link = (
     <Link
       onClick={() => (onClick && onClick()) || {}}
       to={href}
       key={href}
-      className={`${styles.link} ${className}`}
+      className={classNames(styles.link, className, {
+        [styles.disabled]: activeLocation,
+      })}
     >
       {label}
     </Link>
   )
 
-  return location.pathname === href ? <Card>{link}</Card> : link
+  return activeLocation ? <Card>{link}</Card> : link
 }
 export default NavigationBarLink
