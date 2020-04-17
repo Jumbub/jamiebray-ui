448e9fb1a75e8b0922e829f1d9bbbed4fd3e1c92
Jamie Bray <jamie@jamiebray.xyz>
2020-04-18 00:17:46 +0800

    ssr Delete everything

--- src/constants/personal.tsx
+++
@@ -1,6 +0,0 @@
-const FULL_NAME = 'Jamie Bray'
-const EMAIL = 'jamie@jamiebray.xyz'
-const RESUME_PDF_DOWNLOAD =
-  'https://docs.google.com/document/export?format=pdf&id=168aOvH-RFP0cJ5S1cYHCUmvB1hdFnYKTa94WjnaWXco&includes_info_params=true'
-
-export { EMAIL, FULL_NAME, RESUME_PDF_DOWNLOAD }
