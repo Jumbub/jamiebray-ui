ced187a7582c17e263fcf8f5801d4a118d7c12ca
Jamie Bray <jamie@jamiebray.xyz>
2020-04-26 20:32:32 +0800

    Fix linting errors

--- src/gui/constants/personal.tsx
+++ src/gui/constants/personal.tsx
@@ -1,7 +1,9 @@
 export const FULL_NAME = 'Jamie Bray';
+
 export const EMAIL = 'jamie@jamiebray.xyz';
 
 export const RESUME_PDF_DOWNLOAD =
   'https://docs.google.com/document/export?format=pdf&id=168aOvH-RFP0cJ5S1cYHCUmvB1hdFnYKTa94WjnaWXco&includes_info_params=true';
+
 export const RESUME_PDF_IFRAME =
   'https://docs.google.com/document/d/e/2PACX-1vSRoB7vomuVYqdlVbF49GwDtgjuCXpytklI8upjOQ5-CTcZfLutDBeUs7ZtnLsM60ySe-nbupFz1UPK/pub?embedded=true';
