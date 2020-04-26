6323cd2ec750df6107790dc64558028c952f2e0d
Jamie Bray <jamie@jamiebray.xyz>
2020-04-26 19:55:05 +0800

    Preload resume iframe while on other pages

--- src/gui/constants/personal.tsx
+++ src/gui/constants/personal.tsx
@@ -1,7 +1,7 @@
-const FULL_NAME = 'Jamie Bray';
-const EMAIL = 'jamie@jamiebray.xyz';
+export const FULL_NAME = 'Jamie Bray';
+export const EMAIL = 'jamie@jamiebray.xyz';
 
-const RESUME_PDF_DOWNLOAD =
+export const RESUME_PDF_DOWNLOAD =
   'https://docs.google.com/document/export?format=pdf&id=168aOvH-RFP0cJ5S1cYHCUmvB1hdFnYKTa94WjnaWXco&includes_info_params=true';
-
-export { EMAIL, FULL_NAME, RESUME_PDF_DOWNLOAD };
+export const RESUME_PDF_IFRAME =
+  'https://docs.google.com/document/d/e/2PACX-1vSRoB7vomuVYqdlVbF49GwDtgjuCXpytklI8upjOQ5-CTcZfLutDBeUs7ZtnLsM60ySe-nbupFz1UPK/pub?embedded=true';
