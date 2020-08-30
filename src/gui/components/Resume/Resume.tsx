2b917d8821034e01ac49a931888a2701aaaa53b0
Jamie Bray <jamie@jamiebray.xyz>
2020-08-30 23:03:38 +0800

    Revert "Looks like not preloading might be a better option... (#11)"
    This reverts commit b2a8ed1da97097af78de7929533315c5d01ee0c1.

--- src/gui/components/Resume/Resume.tsx
+++ src/gui/components/Resume/Resume.tsx
@@ -10,9 +10,7 @@ import styles from './ContentCardResume.module.scss';
 import { ResumeContext } from './ResumeContext';
 
 const NoOverflow = styled.div`
-  display: inline-block;
   overflow: hidden;
-  width: 50%;
 `;
 
 const ResumeStyle = styled.iframe`
@@ -30,16 +28,8 @@ export const Resume = memo(() => {
   return (
     <Card>
       <NoOverflow>
-        PRELOADED
-        <ResumeStyle key="PRELOADED" srcDoc={`${html || ''}`} sandbox="" />
-      </NoOverflow>
-      <NoOverflow>
-        SOURCED
-        <ResumeStyle
-          key="SOURCED"
-          src="https://docs.google.com/document/d/e/2PACX-1vTiu0_Xy7H6CtthFAXeyHfwV7qxtHaJXS3a7IFzIoUb9dlE24Q8Xs0aLFyJe57Y-rcXsSY7xrtRTjt_/pub?embedded=true"
-          sandbox=""
-        />
+        {!html && <ResumeStyle sandbox="" />}
+        {!!html && <ResumeStyle key="RESUME" srcDoc={`${html}`} sandbox="" />}
       </NoOverflow>
     </Card>
   );
