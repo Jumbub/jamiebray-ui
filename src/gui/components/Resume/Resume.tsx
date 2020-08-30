056498c7a09bd9c2f255953bf46a94396a96701a
Jamie Bray <jamie@jamiebray.xyz>
2020-08-30 22:36:22 +0800

    Looks like not preloading might be a better option...

--- src/gui/components/Resume/Resume.tsx
+++ src/gui/components/Resume/Resume.tsx
@@ -11,6 +11,8 @@ import { ResumeContext } from './ResumeContext';
 
 const NoOverflow = styled.div`
   overflow: hidden;
+  display: inline-block;
+  width: 50%;
 `;
 
 const ResumeStyle = styled.iframe`
@@ -28,8 +30,18 @@ export const Resume = memo(() => {
   return (
     <Card>
       <NoOverflow>
-        {!html && <ResumeStyle sandbox="" />}
-        {!!html && <ResumeStyle key="RESUME" srcDoc={`${html}`} sandbox="" />}
+        PRELOADED
+        <ResumeStyle key="PRELOADED" srcDoc={`${html || ''}`} sandbox="" />
+      </NoOverflow>
+      <NoOverflow>
+        SOURCED
+        <ResumeStyle
+          key="SOURCED"
+          src={
+            'https://docs.google.com/document/d/e/2PACX-1vTiu0_Xy7H6CtthFAXeyHfwV7qxtHaJXS3a7IFzIoUb9dlE24Q8Xs0aLFyJe57Y-rcXsSY7xrtRTjt_/pub?embedded=true'
+          }
+          sandbox=""
+        />
       </NoOverflow>
     </Card>
   );
