4d3942cab1500800bd09e808032956bf2365f70a
Jamie Bray <jamie@jamiebray.xyz>
2020-08-31 00:27:29 +0800

    Improve mobile friendliness of the resume

--- src/gui/components/ContentCardResume/ContentCardResume.tsx
+++ src/gui/components/ContentCardResume/ContentCardResume.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import { FaDownload } from 'react-icons/fa';
-import { RESUME_PDF_DOWNLOAD } from '../../constants/personal';
+import { FaDownload, FaEye } from 'react-icons/fa';
+import { RESUME_PDF_DOWNLOAD, RESUME_PDF_LINK, RESUME_PDF_IFRAME } from '../../constants/personal';
 import { Card } from '../Card/Card';
 import Content from '../Content/Content';
 import { Head } from '../Head/Head';
 import IconCard from '../IconCard/IconCard';
-import { Resume } from '../Resume/Resume';
-import styles from './ContentCardResume.module.scss';
+import styled from 'styled-components';
+import { ShowOnDesktop } from '../ShowOnDesktop/ShowOnDesktop';
+import { ShowOnMobile } from '../ShowOnMobile/ShowOnMobile';
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  flex-direction: row-reverse;
+  height: 0;
+  width: 100%;
+  top: 16px;
+  right: -8px;
+`;
+
+const NoOverflow = styled.div`
+  overflow: hidden;
+`;
+
+export const DesktopResumeStyle = styled.iframe`
+  border: none;
+  height: 1170px;
+  margin: 10px;
+  max-width: 900px;
+  width: 100%;
+`;
+
+export const MobileResumeStyle = styled.iframe`
+  border: none;
+  width: calc(100% + 48px);
+  height: calc(100vh - 12vh);
+  margin: 0 -24px;
+`;
 
 export const ContentCardResume = () => (
   <>
     <Head title="Resume - Jamie Bray" />
     <Card>
-      <Content title="Resume">
-        <div className={styles.textAndAction}>
-          <p>You can view my resume here, or download it!</p>
-          <a href={RESUME_PDF_DOWNLOAD} title="Download Resume PDF">
-            <IconCard render={className => <FaDownload className={className} />} />
-          </a>
-        </div>
-        <Resume />
-      </Content>
+      <ShowOnDesktop>
+        <Content title="Resume">
+          <ShowOnDesktop>
+            <IconContainer>
+              <DownloadResume />
+            </IconContainer>
+            <NoOverflow>
+              <DesktopResumeStyle src={RESUME_PDF_IFRAME} sandbox="" />
+            </NoOverflow>
+          </ShowOnDesktop>
+        </Content>
+      </ShowOnDesktop>
+
+      <ShowOnMobile>
+        <IconContainer>
+          <DownloadResume />
+        </IconContainer>
+        <NoOverflow>
+          <MobileResumeStyle src={RESUME_PDF_IFRAME} sandbox="" />
+        </NoOverflow>
+      </ShowOnMobile>
     </Card>
   </>
+);
+
+const DownloadResume = () => (
+  <a href={RESUME_PDF_DOWNLOAD} title="Download Resume PDF">
+    <IconCard render={className => <FaDownload className={className} />} />
+  </a>
 );
--- src/gui/constants/personal.tsx
+++ src/gui/constants/personal.tsx
@@ -5,5 +5,8 @@ export const EMAIL = 'jamie@jamiebray.xyz';
 export const RESUME_PDF_DOWNLOAD =
   'https://docs.google.com/document/export?format=pdf&id=168aOvH-RFP0cJ5S1cYHCUmvB1hdFnYKTa94WjnaWXco&includes_info_params=true';
 
+export const RESUME_PDF_LINK =
+  'https://docs.google.com/document/d/e/2PACX-1vSRoB7vomuVYqdlVbF49GwDtgjuCXpytklI8upjOQ5-CTcZfLutDBeUs7ZtnLsM60ySe-nbupFz1UPK/pub';
+
 export const RESUME_PDF_IFRAME =
   'https://docs.google.com/document/d/e/2PACX-1vSRoB7vomuVYqdlVbF49GwDtgjuCXpytklI8upjOQ5-CTcZfLutDBeUs7ZtnLsM60ySe-nbupFz1UPK/pub?embedded=true';
