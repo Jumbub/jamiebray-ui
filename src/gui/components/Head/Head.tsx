423e4642de9533d93853112d7e5d86b95258117f
Jamie Bray <jamie@jamiebray.xyz>
2020-09-07 00:27:34 +0800

    Improve the website description

--- src/gui/components/Head/Head.tsx
+++ src/gui/components/Head/Head.tsx
@@ -4,7 +4,10 @@ export const Head = ({ title }: { title: string }) => (
   <NextHead>
     <title>{title}</title>
     <meta name="author" content="Jamie Bray" />
-    <meta name="description" content="Personal online resume" />
+    <meta
+      name="description"
+      content="Jamie Bray's online resume. Full stack developer at HealthEngine. Working experience with React, Typescript, Next.js, Storybook, GraphQL, AWS, Docker, Laravel."
+    />
     <meta name="keywords" content="Personal, Resume, Hire Me, Programmer, Developer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </NextHead>
